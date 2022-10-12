f04354e652f9c6a57ae40e751276b809b0b1adb1
Robson Lima <rsl.robson@gmail.com>
2022-10-12 17:16:03 -0300

    Verifying if the ViewModel preserves data.

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -8,9 +8,11 @@ class GameViewModel : ViewModel() {
     private var _score = 0
     val score: Int
         get() = _score
+
     private var _currentWordCount = 0
     val currentWordCount: Int
         get() = _currentWordCount
+
     private lateinit var _currentScrambledWord: String
     val currentScrambledWord: String
         get() = _currentScrambledWord
