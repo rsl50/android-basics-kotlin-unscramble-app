d8cbf4bd7ae9d2b04caf4484895a2da8730f49fa
Robson Lima <rsl.robson@gmail.com>
2022-10-12 17:19:46 -0300

    Updating game restart logic to reset score.

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -133,6 +133,7 @@ class GameFragment : Fragment() {
      * restart the game.
      */
     private fun restartGame() {
+        viewModel.reinitializeData()
         setErrorTextField(false)
         updateNextWordOnScreen()
     }
